a2112f11def7932019018c5df221443310a7a55e
AlexRudnyk <alexander.rudnyk@ukr.net>
2025-03-21 15:22:05 +0200

    added suspense for useSearchParams

--- src/components/PriceSlider/PriceSlider.tsx
+++ src/components/PriceSlider/PriceSlider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, Suspense, useState } from "react";
 import Slider from "@mui/material/Slider";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -41,18 +41,20 @@ const PriceSlider: FC<Props> = ({ prices }) => {
   };
 
   return (
-    <div style={{ padding: "30px", width: "300px" }}>
-      <Slider
-        getAriaLabel={() => "Minimum distance"}
-        value={value}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        disableSwap
-        min={minPrice}
-        max={maxPrice}
-        step={50}
-      />
-    </div>
+    <Suspense>
+      <div style={{ padding: "30px", width: "300px" }}>
+        <Slider
+          getAriaLabel={() => "Minimum distance"}
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          disableSwap
+          min={minPrice}
+          max={maxPrice}
+          step={50}
+        />
+      </div>
+    </Suspense>
   );
 };
 
