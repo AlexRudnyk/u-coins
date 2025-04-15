54be095dab426acc35b99f7b150d37ef59537582
AlexRudnyk <alexander.rudnyk@ukr.net>
2025-04-15 21:45:28 +0300

    added loader

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,13 +1,30 @@
 "use client";
 
 import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useSearchParams } from "next/navigation";
 
+import BreadCrumbs from "../BreadCrumbs";
 import CoinsListItem from "../CoinsListItem";
 
 import s from "./SearchResults.module.scss";
 
+import { routes } from "@/helpers/routes";
 import { useQueryFilteredCoins } from "@/hooks/useQueryCoins";
+import { Crumb } from "@/types/crumbs";
+
+const breadCrumbs: Crumb[] = [
+  {
+    id: 1,
+    label: "Home",
+    link: routes.home,
+  },
+  {
+    id: 2,
+    label: "Search",
+    link: "",
+  },
+];
 
 const SearchResults = () => {
   const searchParams = useSearchParams();
@@ -16,19 +33,32 @@ const SearchResults = () => {
   const toPrice = "";
   const q = searchParams.get("q") || "";
 
-  const { data: coins } = useQueryFilteredCoins(fromPrice, toPrice, q);
-
-  if (!coins) return;
-
-  console.log("RESULT", coins);
+  const { data: coins, isPending } = useQueryFilteredCoins(
+    fromPrice,
+    toPrice,
+    q
+  );
 
   return (
     <div className={s.container}>
-      <ul className={s.coinsList}>
-        {coins?.map((coin) => (
-          <CoinsListItem key={coin._id} coin={coin} />
-        ))}
-      </ul>
+      <BreadCrumbs crumbs={breadCrumbs} />
+      {isPending ? (
+        <div className={s.flexWrapper}>
+          <CircularProgress />
+        </div>
+      ) : coins && coins.length > 0 ? (
+        <ul className={s.coinsList}>
+          {coins?.map((coin) => (
+            <CoinsListItem key={coin._id} coin={coin} />
+          ))}
+        </ul>
+      ) : (
+        <div className={s.flexWrapper}>
+          <p className={s.notFoundText}>
+            There is nothing found. Please specify your request
+          </p>
+        </div>
+      )}
     </div>
   );
 };
