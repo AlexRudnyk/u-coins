a666354180e39458b31ecd004a91287f84291092
AlexRudnyk <alexander.rudnyk@ukr.net>
2025-04-16 14:34:42 +0300

    added suspense to search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
+
 import SearchResults from "@/components/SearchResults";
 
 export default function Page() {
-  return <SearchResults />;
+  return (
+    <Suspense>
+      <SearchResults />
+    </Suspense>
+  );
 }
